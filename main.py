a0a0e17b5d3d2a13bf409b9fed96941890d6bfd8
Kamel <ahmdkamel432bs@gmail.com>
2026-04-21 19:32:33 +0200

    first agent: search agent

---
+++ main.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_ollama import ChatOllama
+from tavily import TavilyClient
+
+load_dotenv()
+tavily = TavilyClient()  
+
+
+@tool
+def search(query: str) -> str:
+    """
+    Tool that searches the web.
+    Args:
+        query: The search query.
+    Returns:
+        The search results.
+    """
+    print(f"Searching for: {query}")
+    return tavily.search(query=query) 
+
+llm = ChatOllama(temperature=0, model="llama3.1:8b")
+tools = [search]
+agent = create_agent(model=llm, tools=tools)
+
+
+def main():
+    print("Hello from langchain-course!")
+    result = agent.invoke({"messages": [HumanMessage(content="search for 3 postings for .net developer positions in Egypt on linkedin and list their details")]})
+    print(f"Agent response: {result}")
+
+
+if __name__ == "__main__":
+    main()
